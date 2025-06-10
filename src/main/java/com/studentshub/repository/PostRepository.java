d85378861dd8ee8023372481bd2508fa8a18a649
Ljubica <lubicaj724@gmail.com>
2025-06-10 13:05:02 +0200

    Home page logic started

--- src/main/java/com/studentshub/repository/PostRepository.java
+++ src/main/java/com/studentshub/repository/PostRepository.java
@@ -2,12 +2,15 @@ package com.studentshub.repository;
 
 import com.studentshub.model.Post;
 import com.studentshub.model.User;
+import com.studentshub.model.enumerations.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByOwner(User owner);
+    Optional<Post> findTopByCategoryOrderByCreatedAtDesc(PostCategory category);
 
 }
 
