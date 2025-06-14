73d5e9885cd0dcb38cb731e117a0f730263a6ac5
Ljubica Jovanoska <lubicaj724@gmail.com>
2025-06-14 13:15:38 +0200

    Problem with favourites resolved

--- src/main/java/com/studentshub/web/ProfileController.java
+++ src/main/java/com/studentshub/web/ProfileController.java
@@ -73,6 +73,7 @@ public class ProfileController {
             user.setLastName(userDto.getLastName());
             user.setEmail(userDto.getEmail());
             user.setEducation(userDto.getEducation());
+            user.setUsername(userDto.getUsername());
 
             // Ако има слика, сними ја
             if (profileImage != null && !profileImage.isEmpty()) {
